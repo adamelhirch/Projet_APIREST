6ed5e210b466f23605ed5c26e20859c3173eaba6
Adam EL HIRCH <adamelhirch05@gmail.com>
2025-03-13 11:51:41 +0100

    test

--- api/connexionDB.php
+++ api/connexionDB.php
@@ -3,7 +3,7 @@ $host = 'localhost';
 $dbname = 'projetapi';
 $username = 'root';
 $password = '';
-
+//
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
